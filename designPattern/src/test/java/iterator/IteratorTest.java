e6918bb7243a4ca4d2bef9490c0144708a6216d9
wehwan <we_hwan@naver.com>
2020-11-11 22:41:28 +0900

    remote save.

---
+++ designPattern/src/test/java/iterator/IteratorTest.java
@@ -0,0 +1,16 @@
+package iterator;
+
+import org.example.designpattern.iterator.Book;
+
+public class IteratorTest {
+
+    public static void main(String[] args) {
+
+        // given
+        Book book1 = new Book("design pattern 1");
+        Book book2 = new Book("design pattern 2");
+        Book book3 = new Book("design pattern 3");
+
+        // when
+    }
+}
