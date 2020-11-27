ebc9b7b5594a8f78e762c801c4aef119eb6cd96d
wehwan <we_hwan@naver.com>
2020-11-27 17:01:24 +0900

    feat: change to WebApplication not running

--- extract.value/src/main/java/kr/co/crossarc/extract/value/Application.java
+++ extract.value/src/main/java/kr/co/crossarc/extract/value/Application.java
@@ -1,13 +1,16 @@
 package kr.co.crossarc.extract.value;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication springApplication = new SpringApplication(Application.class);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+		springApplication.run(args);
 	}
 
 }
