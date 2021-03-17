4958718cc85aa22a6b59246d0e66d064bc284a32
wehwan <we_hwan@naver.com>
2021-03-17 14:19:11 +0900

    refactor: 아이템5 예제 수정

--- effective-java-3e-source-code-master/src/effectivejava/chapter2/item5/SupplierExam.java
+++ effective-java-3e-source-code-master/src/effectivejava/chapter2/item5/SupplierExam.java
@@ -1,23 +1,38 @@
 package effectivejava.chapter2.item5;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 public class SupplierExam {
 
-    Mosaic create(Supplier<? extends Tile> tileFactory) {
-        Tile tile = tileFactory.get();
-        return new Mosaic(tile);
-    }
-
     public static void main(String[] args) {
-        SupplierExam exam = new SupplierExam();
-        Mosaic 타일2모자이크 = exam.create(() -> new CustomTile("타일2"));
+        Mosaic 기본타일_모자이크 = Mosaic.create(new Tile());
+
+        // 사용자가 만든 팩토리를 받아서 처리 할 수 있다
+        Supplier<Tile> tileFactory = () -> {
+            String randomTileName = "타일" + new Random().nextInt(100);
+            return new CustomTile(randomTileName);
+        };
+        Mosaic 타일2_모자이크 = Mosaic.create(tileFactory);
+        Mosaic 타일2_모자이크2 = Mosaic.create(() -> new CustomTile("타일2"));
     }
 }
 
 class Mosaic {
+    private final Tile tile;
+
     public Mosaic(Tile tile) {
         System.out.println("create mosaic by " + tile);
+        this.tile = tile;
+    }
+
+    static Mosaic create(Supplier<? extends Tile> tileFactory) {
+        Tile tile = tileFactory.get();
+        return new Mosaic(tile);
+    }
+
+    static Mosaic create(Tile tile) {
+        return new Mosaic(tile);
     }
 }
 
